5316cb7bd3cce61ec48c72d2b96154b703c12907
G3kid0 <73354357+G3kid0@users.noreply.github.com>
2025-04-10 20:53:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+
+// Configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBGXjguIegEK6bZl_u7kzBUVku8oJXcCPM",
+  authDomain: "frenchies-4d63a.firebaseapp.com",
+  databaseURL: "https://frenchies-4d63a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "frenchies-4d63a",
+  storageBucket: "frenchies-4d63a.appspot.com",
+  messagingSenderId: "270761793230",
+  appId: "1:270761793230:web:b4914660a81023dce1229f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// 🔧 Ton code existant
 const addIconButton = document.getElementById('addIcon');
 const container = document.getElementById('iconContainer');
 
@@ -9,11 +27,61 @@ const closeBtn = document.getElementById('closePopup');
 
 let currentIcon = null;
 
+function generateId() {
+  return 'icon-' + Math.random().toString(36).substring(2, 9);
+}
+
+function saveIcon(id, data) {
+  set(ref(db, 'icons/' + id), data);
+}
+
+function makeDraggable(el, id) {
+  let offsetX, offsetY;
+
+  const onMove = (e) => {
+    const x = e.touches ? e.touches[0].clientX : e.clientX;
+    const y = e.touches ? e.touches[0].clientY : e.clientY;
+    el.style.left = (x - offsetX) + 'px';
+    el.style.top = (y - offsetY) + 'px';
+  };
+
+  const onUp = () => {
+    document.removeEventListener('mousemove', onMove);
+    document.removeEventListener('mouseup', onUp);
+    document.removeEventListener('touchmove', onMove);
+    document.removeEventListener('touchend', onUp);
+
+    // Sauvegarder position
+    const x = el.style.left;
+    const y = el.style.top;
+    const img = el.querySelector('img')?.src || '';
+    const text = el.querySelector('span')?.textContent || '';
+    saveIcon(id, { x, y, image: img, text });
+  };
+
+  el.addEventListener('mousedown', (e) => {
+    offsetX = e.offsetX;
+    offsetY = e.offsetY;
+    document.addEventListener('mousemove', onMove);
+    document.addEventListener('mouseup', onUp);
+  });
+
+  el.addEventListener('touchstart', (e) => {
+    const rect = el.getBoundingClientRect();
+    offsetX = e.touches[0].clientX - rect.left;
+    offsetY = e.touches[0].clientY - rect.top;
+    document.addEventListener('touchmove', onMove);
+    document.addEventListener('touchend', onUp);
+  });
+}
+
 addIconButton.addEventListener('click', () => {
+  const id = generateId();
   const icon = document.createElement('div');
   icon.className = 'icon';
   icon.style.left = '100px';
   icon.style.top = '100px';
+  icon.dataset.id = id;
 
   icon.addEventListener('click', (e) => {
     e.stopPropagation();
@@ -25,14 +93,13 @@ addIconButton.addEventListener('click', () => {
     popup.classList.remove('hidden');
   });
 
-  makeDraggable(icon);
+  makeDraggable(icon, id);
   container.appendChild(icon);
+  saveIcon(id, { x: '100px', y: '100px', image: '', text: '' });
 });
 
 applyBtn.addEventListener('click', () => {
   if (!currentIcon) return;
-
-  // Supprimer l'ancienne image ou span s'ils existent
   currentIcon.querySelector('img')?.remove();
   currentIcon.querySelector('span')?.remove();
 
@@ -51,6 +118,11 @@ applyBtn.addEventListener('click', () => {
     currentIcon.appendChild(span);
   }
 
+  const id = currentIcon.dataset.id;
+  const x = currentIcon.style.left;
+  const y = currentIcon.style.top;
+  saveIcon(id, { x, y, image: url, text });
+
   popup.classList.add('hidden');
   currentIcon = null;
 });
@@ -59,36 +131,3 @@ closeBtn.addEventListener('click', () => {
   popup.classList.add('hidden');
   currentIcon = null;
 });
-
-function makeDraggable(el) {
-  let offsetX, offsetY;
-
-  const onMove = (e) => {
-    const x = e.touches ? e.touches[0].clientX : e.clientX;
-    const y = e.touches ? e.touches[0].clientY : e.clientY;
-    el.style.left = (x - offsetX) + 'px';
-    el.style.top = (y - offsetY) + 'px';
-  };
-
-  const onUp = () => {
-    document.removeEventListener('mousemove', onMove);
-    document.removeEventListener('mouseup', onUp);
-    document.removeEventListener('touchmove', onMove);
-    document.removeEventListener('touchend', onUp);
-  };
-
-  el.addEventListener('mousedown', (e) => {
-    offsetX = e.offsetX;
-    offsetY = e.offsetY;
-    document.addEventListener('mousemove', onMove);
-    document.addEventListener('mouseup', onUp);
-  });
-
-  el.addEventListener('touchstart', (e) => {
-    const rect = el.getBoundingClientRect();
-    offsetX = e.touches[0].clientX - rect.left;
-    offsetY = e.touches[0].clientY - rect.top;
-    document.addEventListener('touchmove', onMove);
-    document.addEventListener('touchend', onUp);
-  });
-}
